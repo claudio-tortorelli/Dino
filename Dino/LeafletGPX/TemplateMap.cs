b9a4f964d69222ef90c2025242672d02fd71174e
claudio-tortorelli <torto78@gmail.com>
2020-03-14 16:47:26 +0100

    upd

--- Dino/LeafletGPX/TemplateMap.cs
+++ Dino/LeafletGPX/TemplateMap.cs
@@ -67,7 +67,7 @@ namespace Dino
         var Esri_WorldTopoMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
             attribution: '&copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
         });
-
+ 
         var miniMap = new L.Control.MiniMap(Esri_WorldTopoMap, {
             toggleDisplay: true,
             minimized: false,
